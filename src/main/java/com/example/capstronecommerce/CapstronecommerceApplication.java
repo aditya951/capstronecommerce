6b779363643e5dec2e751054fbac9a54b6ec8f2c
aditya <adityajatav95@gmail.com>
2023-12-24 21:42:51 +0530

    just started

---
+++ src/main/java/com/example/capstronecommerce/CapstronecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstronecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstronecommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstronecommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/capstronecommerce/CapstronecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstronecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstronecommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
